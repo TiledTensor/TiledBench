b5c1ae36277ef2e10abc69f077229d81ccf2f730
KuangjuX <18630816527@163.com>
2024-11-10 14:08:56 +0000

    Add Cublas bench.

--- benchs/gemm/bench.py
+++ benchs/gemm/bench.py
@@ -12,6 +12,9 @@ sys.path.insert(0, tiledcuda_dir)
 
 from cutlass.gemm import gemm_func as cutlass_gemm
 from tiledcuda.gemm import gemm_func as tiledcuda_gemm
+from cuBLAS import cublas_gemm
+
+
 
 def run_tiledcuda_unittest(
         a: Tensor,
@@ -45,7 +48,22 @@ def run_tiledcuda_unittest(
     else:
         return True
 
-def run_cutlass_test(
+def run_cublas_bench(
+    a: Tensor,
+    b: Tensor,
+    c: Tensor,
+    M: int,
+    N: int,
+    K: int,
+    time: Tensor
+):
+    warmup = 10
+    iters = 50
+    cublas_gemm(M, N, K, a, b, c, time, iters, warmup)
+
+    return time
+
+def run_cutlass_bench(
     a: Tensor,
     b: Tensor,
     c: Tensor,
@@ -129,10 +147,13 @@ if __name__ == "__main__":
 
     kRK = 32
 
-    cutlass_time = run_cutlass_test(a, b, c, kM, kN, kK, kTM, kTN, kTK, (2, 2))
+    cublas_time = torch.zeros(1, device=torch.device("cpu"), dtype=torch.float32)
+    
+    cublas_gemm = run_cublas_bench(a, b, c, kM, kN, kK, cublas_time)
+    cutlass_time = run_cutlass_bench(a, b, c, kM, kN, kK, kTM, kTN, kTK, (2, 2))
     tiledcuda_time = run_tiledcuda_bench(a, b, c, kM, kN, kK, kTM, kTN, kTK, kRK, (2, 2))
 
     # print("Elapsed time: {:.4f} ms".format(time))
 
     # print(f"cutlass_time: {cutlass_time}, tiledcuda_time: {tiledcuda_time}")
-    print("cutlass_time: {:.4f} ms, tiledcuda_time: {:.4f} ms".format(cutlass_time, tiledcuda_time))
+    print("cublas_time: {:.4f} ms, cutlass_time: {:.4f} ms, tiledcuda_time: {:.4f} ms".format(cublas_time.item(), cutlass_time, tiledcuda_time))

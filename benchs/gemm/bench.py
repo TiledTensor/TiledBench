50a35b24c12ba70c3d363bb8c98f7daee61b1bf9
KuangjuX <18630816527@163.com>
2024-11-12 03:18:24 +0000

    Update Benchmarkmark..

--- benchs/gemm/bench.py
+++ benchs/gemm/bench.py
@@ -14,7 +14,33 @@ from cutlass.gemm import gemm_func as cutlass_gemm
 from tiledcuda.gemm import gemm_func as tiledcuda_gemm
 from cuBLAS import cublas_gemm
 
+def run_cublas_unittest(
+        a: Tensor,
+        b: Tensor,
+        c: Tensor,
+        M: int,
+        N: int,
+        K: int,
+        debug_print=False,
+        epsilon: float = 5e-2
+):
+    time = torch.zeros(1, device=torch.device("cpu"), dtype=torch.float32)
+    cublas_gemm(M, N, K, a, b, c, time)
+    ref_c = a @ b.t()
 
+    if debug_print:
+        print("Result:")
+        print(c)
+
+        print("\nReference:")
+        print(ref_c)
+
+    avg_diff = (torch.sum(torch.abs(ref_c - c) / (M * N))).item()
+
+    if avg_diff > epsilon:
+        return False
+    else:
+        return True
 
 def run_tiledcuda_unittest(
         a: Tensor,
@@ -48,6 +74,37 @@ def run_tiledcuda_unittest(
     else:
         return True
 
+def run_cutlass_unittest(
+        a: Tensor,
+        b: Tensor,
+        c: Tensor,
+        M: int,
+        N: int,
+        K: int,
+        kTM: int,
+        kTN: int,
+        kTK: int,
+        warp_layout: Tuple,
+        debug_print=False,
+        epsilon: float = 5e-2
+):
+    cutlass_gemm(a, b, c, M, N, K, kTM, kTN, kTK, *warp_layout)
+    ref_c = a @ b.t()
+
+    if debug_print:
+        print("Result:")
+        print(c)
+
+        print("\nReference:")
+        print(ref_c)
+
+    avg_diff = (torch.sum(torch.abs(ref_c - c) / (M * N))).item()
+
+    if avg_diff > epsilon:
+        return False
+    else:
+        return True
+
 def run_cublas_bench(
     a: Tensor,
     b: Tensor,
@@ -57,11 +114,21 @@ def run_cublas_bench(
     K: int,
     time: Tensor
 ):
-    warmup = 10
+    # Run unittest
+    if run_cublas_unittest(a, b, c, M, N, K):
+        # print("Unittest passed")
+        pass
+    else:
+        # raise ValueError("Unittest failed")
+        print("Run cuBLAS unittest failed")
+        return float("NaN")
+
     iters = 50
+    warmup = 10
+
     cublas_gemm(M, N, K, a, b, c, time, iters, warmup)
 
-    return time
+    return time.item()
 
 def run_cutlass_bench(
     a: Tensor,
@@ -75,16 +142,24 @@ def run_cutlass_bench(
     kTK: int,
     warp_layout: Tuple,
 ):
+    if run_cutlass_unittest(a, b, c, M, N, K, kTM, kTN, kTK, warp_layout):
+        # print("Unittest passed")
+        pass
+    else:
+        # raise ValueError("Unittest failed")
+        print("Run Cutlass unittest failed")
+        return float("NaN")
 
-    # if run_unittest(a, b, c, M, N, K, kTM, kTN, kTK, warp_layout):
-    #     print("Unittest passed")
-    # else:
-    #     raise ValueError("Unittest failed")
+    warmup = 10
+    iters = 50
+
+    for _ in range(warmup):
+        cutlass_gemm(a, b, c, M, N, K, kTM, kTN, kTK, *warp_layout)
+    
 
     start_event = torch.cuda.Event(enable_timing=True)
     end_event = torch.cuda.Event(enable_timing=True)
 
-    iters = 50
     start_event.record()
     for _ in range(iters):
         cutlass_gemm(a, b, c, M, N, K, kTM, kTN, kTK, *warp_layout)
@@ -109,16 +184,22 @@ def run_tiledcuda_bench(
     kRK: int,
     warp_layout: Tuple,
 ):
-
     if run_tiledcuda_unittest(a, b, c, M, N, K, kTM, kTN, kTK, kRK, warp_layout):
-        print("Unittest passed")
+        pass
     else:
-        raise ValueError("Unittest failed")
+        # raise ValueError("Unittest failed")
+        print("Run TiledCUDA unittest failed")
+        return float('NaN')
+
+    warmup = 10
+    iters = 50
+
+    for _ in range(warmup):
+        tiledcuda_gemm(a, b, c, M, N, K, kTM, kTN, kTK, kRK, *warp_layout)
 
     start_event = torch.cuda.Event(enable_timing=True)
     end_event = torch.cuda.Event(enable_timing=True)
 
-    iters = 50
     start_event.record()
     for _ in range(iters):
         tiledcuda_gemm(a, b, c, M, N, K, kTM, kTN, kTK, kRK, *warp_layout)
@@ -144,27 +225,41 @@ def run_bench(
     a = torch.randn(M, K, device = 'cuda', dtype = torch.float16)
     b = torch.randn(N, K, device = 'cuda', dtype = torch.float16)
     c = torch.zeros(M, N, device = 'cuda', dtype = torch.float32)
+    half_c = torch.zeros(M, N, device = 'cuda', dtype = torch.float16)
     
-    cublas_time = torch.zeros(1, device=torch.device("cpu"), dtype=torch.float32)
+    cublas_time_tensor = torch.zeros(1, device=torch.device("cpu"), dtype=torch.float32)
 
-    cublas_gemm(M, N, K, a, b, c, cublas_time, 50, 10)
+    cublas_time = run_cublas_bench(a, b, half_c, M, N, K, cublas_time_tensor)
     cutlass_time = run_cutlass_bench(a, b, c, M, N, K, kTM, kTN, kTK, warp_layout)
     tiledcuda_time = run_tiledcuda_bench(a, b, c, M, N, K, kTM, kTN, kTK, kRK, warp_layout)
 
     print("(M, N, K) (kTM, kTN, kTK)")
     print("({}, {}, {}) ({}, {}, {})".format(M, N, K, kTM, kTN, kTK))
-    print("cublas_time: {:.4f} ms, cutlass_time: {:.4f} ms, tiledcuda_time: {:.4f} ms".format(cublas_time.item(), cutlass_time, tiledcuda_time))
+    print("cublas_time: {:.4f} ms, cutlass_time: {:.4f} ms, tiledcuda_time: {:.4f} ms".format(cublas_time, cutlass_time, tiledcuda_time))
 
 
 if __name__ == "__main__":
-    kTM = 64
-    kTN = 256
-    kTK = 32
+    # kTM = 64
+    # kTN = 256
+    # kTK = 32
+
+    kTM = 128
+    kTN = 64
+    kTK = 64
+
 
     kRK = 32
 
-    run_bench(256, 256, 256, kTM, kTN, kTK, kRK, (2, 2))
-    run_bench(1024, 1024, 1024, kTM, kTN, kTK, kRK, (2, 2))
-    run_bench(4096, 4096, 2048, kTM, kTN, kTK, kRK, (2, 2))
+    run_bench(1024, 1024, 1024, 64, 256, 32, kRK, (2, 2))
+    run_bench(4096, 4096, 512, 64, 256, 32, kRK, (2, 2))
+    run_bench(4096, 4096, 2048, 64, 256, 32, kRK, (2, 2))
+
+    run_bench(1024, 1024, 1024, 128, 64, 64, kRK, (2, 2))
+    run_bench(4096, 4096, 512, 128, 64, 64, kRK, (2, 2))
+    run_bench(4096, 4096, 2048, 128, 64, 64, kRK, (2, 2))
+
+    run_bench(1024, 1024, 1024, 128, 128, 64, kRK, (2, 2))
+    run_bench(4096, 4096, 512, 128, 128, 64, kRK, (2, 2))
+    run_bench(4096, 4096, 2048, 128, 128, 64, kRK, (2, 2))
 
     
--- benchs/gemm/cuBLAS/__init__.py
+++ benchs/gemm/cuBLAS/__init__.py
@@ -6,13 +6,15 @@ dyn_lib_path = os.path.join(os.path.dirname(__file__), "src/build/libcublas_gemm
 
 torch.ops.load_library(dyn_lib_path)
 
-def cublas_gemm(m: int,
-         n: int,
-         k: int,
-         a: Tensor,
-         b: Tensor,
-         c: Tensor,
-         elapsed_time: Tensor,
-         iters: int = 0,
-         warmup: int = 0):
+def cublas_gemm(
+        m: int,
+        n: int,
+        k: int,
+        a: Tensor,
+        b: Tensor,
+        c: Tensor,
+        elapsed_time: Tensor,
+        iters: int = 0,
+        warmup: int = 0
+):
     torch.ops.cublas_gemm.gemm(m, n, k, a, b, c, elapsed_time, iters, warmup)

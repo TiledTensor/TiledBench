9c94831eb35f2aed18cb5059488abd56d74ba713
KuangjuX <18630816527@163.com>
2024-11-13 06:43:14 +0000

    fix CMake.

--- benchs/cpp/gemm/bench.cu
+++ benchs/cpp/gemm/bench.cu
@@ -1,3 +1,9 @@
 #include "cutlass/cutlass_gemm.cuh"
 
-int main() {}
+template <typename Element,                             //
+          const int kM, const int kN, const int kK,     //
+          const int kTM, const int kTN, const int kTK,  //
+          const int kWarpPerRow, const int kWarpPerCol>
+void run_test() {}
+
+int main() { printf("Hello World\n"); }
